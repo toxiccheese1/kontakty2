1455712ebff2196e560ce1002f6cefb94d560b33
Henriko <levius.pl@gmail.com>
2024-10-22 09:32:14 +0200

    zadanie1

---
+++ napoje2.js
@@ -0,0 +1,29 @@
+console.log(); 
+
+console.log('zadanie 1');
+console.log('----------------'); 
+
+console.log(); 
+let lista_kontaktow = [
+    {
+        nazwa:"Maisie Haley",
+        telefon: "0913 531 3030",
+        email:"risus.Quisque@urna.ca.",
+    },
+    {
+        nazwa:"Maxwell Wright",
+        telefon:"0866 398 2895",
+        email:"Curabitur.egestas.nonumyac.co.uk",
+
+    },
+    {
+        nazwa:"Raja Villarreal",
+        telefon:"(0191) 719 6495",
+        email:"posuere.vulputate@sed.com"
+    },
+]
+console.log(lista_kontaktow[0].nazwa, lista_kontaktow[0].nazwa, lista_kontaktow[0].nazwa);
+console.log("  ");
+console.log(lista_kontaktow[1].nazwa, lista_kontaktow[1].nazwa, lista_kontaktow[1].nazwa);
+console.log("  ");
+console.log(lista_kontaktow[2].nazwa, lista_kontaktow[2].nazwa, lista_kontaktow[2].nazwa);
